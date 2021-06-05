8f8f8ca070a253f9a0d5dc49438edad61fe818e9
Serg Basin <shoom.pozitiv@gmail.com>
2021-06-05 18:55:22 +0300

    Initial Commit

---
+++ MyTodos_Combine/MyTodos_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodos_CombineApp.swift
+//  MyTodos_Combine
+//
+//  Created by Sergey Basin on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyTodos_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
